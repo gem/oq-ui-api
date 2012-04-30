5069754b9d53ecb8d6686de99ab3c3945194e6a2
bmw <bmwyss@gmail.com>
2012-04-30 11:32:44 +0200

    update geodetic migrations

--- geonode/geodetic/migrations/0001_initial.py
+++ geonode/geodetic/migrations/0001_initial.py
@@ -10,25 +10,25 @@ class Migration(SchemaMigration):
         
         # Adding model 'Geodetic'
         db.create_table('geodetic_geodetic', (
+            ('gid', self.gf('django.db.models.fields.IntegerField')()),
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('lat', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
+            ('long', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
+            ('exx', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
+            ('eyy', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
+            ('exy', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
             ('var_exx', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
+            ('var_eyy', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
             ('var_exy', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
             ('var_eyy', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
-            ('exx_psr', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
-            ('geom', self.gf('django.contrib.gis.db.models.fields.PolygonField')(dim=3)),
-            ('cc_xx_xy', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
             ('cc_xx_yy', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
-            ('eyy', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
-            ('longi', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
-            ('gid', self.gf('django.db.models.fields.IntegerField')()),
-            ('table_id', self.gf('django.db.models.fields.IntegerField')()),
-            ('eyy_psr', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
-            ('cc_yy_xy', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
-            ('lat', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
+            ('cc_xx_xy', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
+            ('cc_xx_xy', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
             ('x_azimuth', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('exx', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
-            ('exy', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
-        ))
+            ('exx_psr', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
+            ('eyy_psr', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
+            ('the_geom', self.gf('django.contrib.gis.db.models.fields.PointField')(dim=2))
+            )
         db.send_create_signal('geodetic', ['Geodetic'])
     
     
@@ -41,24 +41,24 @@ class Migration(SchemaMigration):
     models = {
         'geodetic.geodetic': {
             'Meta': {'object_name': 'Geodetic'},
-            'cc_xx_xy': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
-            'cc_xx_yy': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
-            'cc_yy_xy': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
-            'exx': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
-            'exx_psr': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
-            'exy': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
-            'eyy': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
-            'eyy_psr': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
-            'geom': ('django.contrib.gis.db.models.fields.PolygonField', [], {'dim': '3'}),
             'gid': ('django.db.models.fields.IntegerField', [], {}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'lat': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
-            'longi': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
-            'table_id': ('django.db.models.fields.IntegerField', [], {}),
+            'long': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'exx': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'eyy': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'exy': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
             'var_exx': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
-            'var_exy': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
             'var_eyy': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
-            'x_azimuth': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'})
+            'var_exy': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'cc_xx_yy': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'cc_xx_xy': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'cc_yy_xy': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'x_azimuth': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}
+            'exx_psr': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'eyy_psr': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'the_geom': ('django.contrib.gis.db.models.fields.PointField', [], {'dim': '2'})
+            )
         }
     }
     
