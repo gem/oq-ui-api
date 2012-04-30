3f8eb208e598402c35cd48d29e241d0484828009
bmw <bmwyss@gmail.com>
2012-04-30 11:51:14 +0200

    update geodetic migrations

--- geonode/geodetic/migrations/0001_initial.py
+++ geonode/geodetic/migrations/0001_initial.py
@@ -22,7 +22,6 @@ class Migration(SchemaMigration):
             ('var_eyy', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
             ('cc_xx_yy', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
             ('cc_xx_xy', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
-            ('cc_xx_xy', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
             ('x_azimuth', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
             ('exx_psr', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
             ('eyy_psr', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
