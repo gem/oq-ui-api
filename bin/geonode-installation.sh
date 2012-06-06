7391471eb297035091d3de4aad8c3b3b65a6d0c5
Matteo Nastasi <nastasi@openquake.org>
2012-06-06 17:49:44 +0200

    bin/geonode-installation.sh: fix bug 972202 adding JAVA_HOME definition into geonode.wsgi file

--- bin/geonode-installation.sh
+++ bin/geonode-installation.sh
@@ -14,13 +14,13 @@ export GEM_DJANGO_SCHEMATA_GIT_REPO=git://github.com/tuttle/django-schemata.git
 export GEM_DJANGO_SCHEMATA_GIT_VERS=8f9487b70c9b1508ae70b502b950066147956993
 
 export       GEM_OQ_UI_API_GIT_REPO=git://github.com/gem/oq-ui-api.git
-export       GEM_OQ_UI_API_GIT_VERS=v0.1.0
+export       GEM_OQ_UI_API_GIT_VERS=v0.1.1
 
 export    GEM_OQ_UI_CLIENT_GIT_REPO=git://github.com/gem/oq-ui-client.git
-export    GEM_OQ_UI_CLIENT_GIT_VERS=v0.1.0
+export    GEM_OQ_UI_CLIENT_GIT_VERS=v0.1.1
 
 export GEM_OQ_UI_GEOSERVER_GIT_REPO=git://github.com/gem/oq-ui-geoserver.git
-export GEM_OQ_UI_GEOSERVER_GIT_VERS=v0.1.0
+export GEM_OQ_UI_GEOSERVER_GIT_VERS=v0.1.1
 
 export GEM_DB_NAME="geonode_dev"
 
@@ -36,6 +36,7 @@ export GEM_GN_SETTINGS="/var/lib/geonode/src/GeoNodePy/geonode/settings.py"
 export GEM_GN_URLS="/var/lib/geonode/src/GeoNodePy/geonode/urls.py"
 export GEM_NW_SETTINGS="/etc/geonode/geonetwork/config.xml"
 export GEM_TOMCAT_LOGFILE="/var/log/geonode/tomcat.log"
+export GEM_WSGI_CONFIG="/var/www/geonode/wsgi/geonode.wsgi"
 export NL='
 '
 export TB='	'
@@ -237,6 +238,9 @@ geonode_installation () {
     if [ $? -ne 0 ]; then
         sed -i 's@\(^WSGIDaemonProcess.*$\)@\1:/var/lib/geonode/src/GeoNodePy/geonode@g' /etc/apache2/sites-available/geonode
     fi
+
+    # this fix the bug 972202 to inform jpype module where is the java installation
+    sed -i "s@os.environ\['DJANGO_SETTINGS_MODULE'\] *= *'geonode.settings'@os.environ['DJANGO_SETTINGS_MODULE'] = 'geonode.settings'\nos.environ['JAVA_HOME'] = '/usr/lib/jvm/java-6-openjdk'@g" "$GEM_WSGI_CONFIG"
 
     service tomcat6 restart
     service apache2 restart
