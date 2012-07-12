9d9bef3c4f29e864806b42384addda9fe5342fe8
Matteo Nastasi <nastasi@openquake.org>
2012-07-12 15:35:10 +0200

    update patch for proxy_views

--- bin/proxy_views_fix.sh
+++ bin/proxy_views_fix.sh
@@ -8,14 +8,13 @@
 #
 
 cat <<EOF | patch -p0
---- null	2012-07-12 13:11:32.964981634 +0200
-+++ /var/lib/geonode/src/GeoNodePy/geonode/proxy/views.py	2012-07-12 13:09:49.888983849 +0200
-@@ -3,8 +3,10 @@
+--- null	2012-07-12 15:05:57.000000000 +0200
++++ /var/lib/geonode/src/GeoNodePy/geonode/proxy/views.py	2012-07-12 15:31:22.748093848 +0200
+@@ -3,7 +3,9 @@
  from urlparse import urlsplit
  import httplib2
  from django.conf import settings
 +from django.views.decorators.csrf import csrf_exempt
- 
  
 +@csrf_exempt
  def proxy(request):
